1d107c5340de5f04a1293617525caab0f2fd7fda
odilf <odysseas.maheras@gmail.com>
2025-09-05 16:47:46 +0200

    fix: make lora work from nix (Closes #1)

--- flake.nix
+++ flake.nix
@@ -16,48 +16,38 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        font-to-static = pkgs.writeShellApplication {
-          name = "font-to-static";
-          runtimeInputs = [ pkgs.python3Packages.fonttools ];
-          text = ''
-            src="$1"
-            name="$(basename "$src" '.ttf')"
-            for wght in 100 200 300 400 500 600 700 800 900; do
-              fonttools varLib.mutator -o "$name-$wght.ttf" "$src" wght="$wght"
-            done
-          '';
-        };
-        lora-static = pkgs.lora.overrideAttrs (
-          finalAttrs: prevAttrs: {
-            nativeBuildInputs = prevAttrs.nativeBuildInputs or [ ] ++ [ font-to-static ];
-            preFixup = ''
-              find "$out" -name '*.ttf' -execdir font-to-static '{}' ';'
-            '';
-          }
-        );
       in
       {
         formatter = pkgs.nixfmt-rfc-style;
 
         devShells = {
-          default = pkgs.mkShell {
+          default = pkgs.mkShellNoCC {
             packages = [
               pkgs.typst
               pkgs.tinymist
               pkgs.typstfmt
-              font-to-static
-              lora-static
               pkgs.just
             ];
+
+            TYPST_FONT_PATHS = pkgs.symlinkJoin {
+              name = "typst-fonts";
+              paths = [
+                pkgs.lora
+              ];
+            };
           };
 
-          ci = pkgs.mkShell {
+          ci = pkgs.mkShellNoCC {
             packages = [
               pkgs.typst
-              font-to-static
-              lora-static
             ];
+
+            TYPST_FONT_PATHS = pkgs.symlinkJoin {
+              name = "typst-fonts";
+              paths = [
+                pkgs.lora
+              ];
+            };
           };
         };
       }
